560ac03a7a0434d8a9bf42476005aba420262b6d
kalineid <noob@mail.ustc.edu.cn>
2020-07-13 02:38:54 +0800

    Add customer update view

--- bms/customer/urls.py
+++ bms/customer/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.CustomerListView.as_view(), name='index'),
     path('create', views.CustomerCreateView.as_view(), name='create'),
     path('delete/<int:pk>', views.CustomerDeleteView.as_view(), name='delete'),
+    path('update/<int:pk>', views.CustomerUpdateView.as_view(), name='update'),
 ]
--- bms/customer/views.py
+++ bms/customer/views.py
@@ -30,3 +30,17 @@ class CustomerDeleteView(generic.DeleteView):
     model = Customer
     template_name = 'customer/delete.html'
     success_url = reverse_lazy('customer:index')
+
+
+class CustomerUpdateView(generic.UpdateView):
+    model = Customer
+    template_name = 'customer/create.html'
+    fields = [
+        'phone_number',
+        'address',
+        'contact_name',
+        'contact_phone_number',
+        'contact_email',
+        'contact_relationship',
+    ]
+    success_url = reverse_lazy('customer:index')
